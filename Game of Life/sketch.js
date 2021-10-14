e429b364b33c3b41c7a14251e19166f16460e1f0
AlexWZero <alexrwriston@gmail.com>
2021-10-14 12:11:10 -0600

    Game of life started

---
+++ Game of Life/sketch.js
@@ -0,0 +1,22 @@
+// Game of Life
+
+let grid;
+let gridSize = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createEmpty2DArray(gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+
+}
+
+function createEmpty2DArray(rows, cols) {
+  for (let x=0; x<width; x++) {
+    for (let y=0; y<height; y++) {
+      
+    }
+  }
+}

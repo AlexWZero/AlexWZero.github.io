bc0ad49f1bd086b7ce4a0bb98c4eacb03d8c1273
AlexWZero <alexrwriston@gmail.com>
2021-10-27 12:14:43 -0600

    grid based game update

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -60,22 +60,34 @@ function setup() {
   cellWidth = width/gridSize;
   cellHeight = height/gridSize;
   grid = level3;
-  cellX = Math.floor(mouseX/cellWidth);
-  cellY = Math.floor(mouseY/cellHeight);
+  
 }
 
 function draw() {
   background(220);
   displayGrid();
+}
 
+function keyPressed() {
+  if (key === "2") {
+    grid = level2;
+  }
+  if (key === "3") {
+    grid = level3;
+  }
 }
 
 function mousePressed() {
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
+  cellX = Math.floor(mouseX/cellWidth);
+  cellY = Math.floor(mouseY/cellHeight);
 
   if (grid[cellY][cellX] === 1) {
     grid[cellY][cellX] = 2;
+  }
+  if (grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 3;
   }
 }
 
@@ -83,8 +95,22 @@ function displayGrid() {
   for (let y=0; y<gridSize; y++) {
     for (let x=0; x<gridSize; x++) {
       strokeWeight(0.5);
-      if (grid[cellY][cellX] === 2) {
+      if (grid[y][x] === 2) {
         fill("black");
+      }
+      if (grid[y][x] === 1 || grid[y][x] === 0) {
+        fill("white");
+      }
+      if (grid[y][x] === 3) {
+        fill("black");
+        if (cellWidth > cellHeight) {
+          textSize(cellHeight*0.75);
+        }
+        else if (cellWidth < cellHeight) {
+          textSize(cellHeight*0.75);
+        }
+        textAlign(CENTER, CENTER);
+        text(grid[y][x], x*cellWidth + cellWidth/2, y*cellHeight + cellHeight/2);
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
